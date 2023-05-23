a50cf71847d03c16beef2d38f13d5786f6bc86b8
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-05-23 07:38:09 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Viktoriia Baburkina and is{" "}
+        <a href="https://github.com/ToriCodesVB/react-app-weather" target='blank'>
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }

6eaa4811a5dc88b6fa1ce51de8412bee3e236790
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-05-24 03:57:00 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Viktoriia Baburkina and is{" "}
-        <a href="https://github.com/ToriCodesVB/react-app-weather" target='blank'>
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        
+        <h1><Weather defaultCity="Dubai" /></h1>
+        <footer>
+          This project was coded by Viktoriia Baburkina and is{" "}
+          <a
+            href="https://github.com/ToriCodesVB/react-app-weather"
+            target="blank">
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://admirable-sherbet-98afb4.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer">
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-

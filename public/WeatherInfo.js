fe8069b31ba6cc411001fba3d94d5b8786236ce0
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-05-24 03:00:45 +0300

    Create Weather.js

---
+++ public/WeatherInfo.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function WeatherInfo() {
+    return (
+        <div className="WeatherInfo">
+            <h1></h1>
+            <ul>
+                <li>FormattedDate</li>
+                <li className="text-capitalize"></li>
+            </ul>
+            <div className="row mt-3">
+                <div className="col-6">
+                    <div className="d-flex">
+                        <div>
+                            WeatherIcon
+                        </div>
+
+                        <div>
+                            WeatherTemperature
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Humidity:</li>
+                        <li>Wind:</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}

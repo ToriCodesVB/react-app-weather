a592329c0207dcac5566e00c18e3e8fdfaf826c5
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-05-24 03:13:01 +0300

    Create WeatherIcon and WeatherTemperature files

--- public/WeatherInfo.js
+++ public/WeatherInfo.js
@@ -1,33 +1,36 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherInfo() {
+export default function WeatherInfo(props) {
     return (
-        <div className="WeatherInfo">
-            <h1></h1>
-            <ul>
-                <li><FormattedDate /></li>
-                <li className="text-capitalize"></li>
-            </ul>
-            <div className="row mt-3">
-                <div className="col-6">
-                    <div className="d-flex">
-                        <div>
-                            WeatherIcon
-                        </div>
+      <div className="WeatherInfo">
+        <h1></h1>
+        <ul>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+          <li className="text-capitalize"></li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="d-flex">
+              <div>
+                <WeatherIcon code={props.data.icon} size={52} />
+              </div>
 
-                        <div>
-                            WeatherTemperature
-                        </div>
-                    </div>
-                </div>
-                <div className="col-6">
-                    <ul>
-                        <li>Humidity:</li>
-                        <li>Wind:</li>
-                    </ul>
-                </div>
+              <div>
+                <WeatherTemperature celsius={props.data.temperature} />
+              </div>
             </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity:</li>
+              <li>Wind:</li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
